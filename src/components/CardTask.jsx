fff59708c0f08ce5fffcf52ae4b708fdaa4e9fb1
DominusDrow <galacticovasquez@gmail.com>
2023-01-23 17:54:56 -0600

    make a modular code

---
+++ src/components/CardTask.jsx
@@ -0,0 +1,31 @@
+import { useContext } from "react";
+import { TaskContext } from "../contex/TaskContex";
+
+export const CardTask = ({ task, index }) => {
+  const { deleteTask, editTask } = useContext(TaskContext);
+  return (
+        <div
+          className="bg-gray-700 p-4 mb-4 flex justify-between items-center"
+          key={index}
+        >
+          <div>
+            <h3 className="text-xl font-bold">{task.title}</h3>
+            <p>{task.description}</p>
+          </div>
+          <div>
+            <button
+              className="bg-red-500 text-white p-2 rounded-md mr-2"
+              onClick={() => deleteTask(index)}
+            >
+              Delete
+            </button>
+            <button
+              className="bg-blue-500 text-white p-2 rounded-md"
+              onClick={() => editTask(task, index)}
+            >
+              Edit
+            </button>
+          </div>
+        </div>
+  );
+};
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,35 +1,14 @@
 import { useContext } from "react";
 import { TaskContext } from "../contex/TaskContex";
+import { CardTask } from "./CardTask";
 
 export const Tasks = () => {
-  const { tasks, deleteTask, editTask } = useContext(TaskContext);
+  const { tasks } = useContext(TaskContext);
 
   return (
     <div className="w-1/2">
       {tasks.map((task, index) => (
-        <div
-          className="bg-gray-700 p-4 mb-4 flex justify-between items-center"
-          key={index}
-        >
-          <div>
-            <h3 className="text-xl font-bold">{task.title}</h3>
-            <p>{task.description}</p>
-          </div>
-          <div>
-            <button
-              className="bg-red-500 text-white p-2 rounded-md mr-2"
-              onClick={() => deleteTask(index)}
-            >
-              Delete
-            </button>
-            <button
-              className="bg-blue-500 text-white p-2 rounded-md"
-              onClick={() => editTask(task, index)}
-            >
-              Edit
-            </button>
-          </div>
-        </div>
+        <CardTask task={task} index={index} key={index} />
       ))}
     </div>
   );
