c6975fc1fb4fce4ce500cb67d79a050954587503
DominusDrow <galacticovasquez@gmail.com>
2023-02-13 16:09:12 -0600

    build the form to add task complete

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -2,9 +2,11 @@ import { useState, useContext } from "react";
 import { TaskContext } from "../contex/TaskContex";
 import ColorRadioButtons from "./radiobuttom";
 import HorizontalInputList from "./TimeInput";
+import { BsPlusLg } from "react-icons/bs";
 
 export const TaskForm = () => {
   const { addTask } = useContext(TaskContext);
+  const [isOpen, setIsOpen] = useState(false);
 
   const [task, setTask] = useState({
     idDate: "",
@@ -30,6 +32,7 @@ export const TaskForm = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    setIsOpen(!isOpen);
 
     task.idDate = Date.now();
     task.priority = priority;
@@ -47,8 +50,25 @@ export const TaskForm = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div className="flex justify-between">
+    <>
+     <button className="bg-gray-700 text-white p-2 rounded-full" onClick={() => setIsOpen(!isOpen)}>
+        <BsPlusLg />
+      </button>
+
+      {isOpen && (
+        <div className="fixed top-0 left-0 w-full h-full bg-gray-900 bg-opacity-50 flex items-center justify-center">
+          <div className="m-auto max-w-xl bg-gray-700 rounded-lg shadow-lg p-4 w-full">
+            <div className="flex justify-center items-center">
+              <h3 className="text-lg font-bold text-white">Add Task</h3>
+              <button
+                className="text-white text-2xl font-bold ml-auto hover:text-gray-900"
+                onClick={() => setIsOpen(!isOpen)}
+              >
+                ×
+              </button>
+            </div>
+            <form className="mb-3" onSubmit={handleSubmit}>
+              <div className="flex justify-between">
         <input
           type="text"
           name="title"
@@ -76,6 +96,11 @@ export const TaskForm = () => {
 
       <button type="submit" className="w-full p-2 bg-green-500 text-white mt-6"> Add</button>
     </form>
+  </div>
+</div>
+)}
+
+    </>
   );
 }
 
--- src/components/pricipalScreen/DocTask.jsx
+++ src/components/pricipalScreen/DocTask.jsx
@@ -5,8 +5,6 @@ export const DocTask = () => {
   return (
     <div className="bg-gray-900 text-white min-h-screen flex flex-col justify-center items-center">
       <div className="w-1/2">
-        <h1 className="text-4xl font-bold text-center mb-4">Task Zenith</h1>
-        <p className="text-center mb-4">This is a sample productivity app for you day a day </p>
         <TaskForm />
         <Tasks />
       </div>
